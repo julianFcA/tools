fe5188d9921d142ba8f24ffd54f120204db1d4ef
Brayan Cardozo <brayancardozod@gmail.com>
2024-05-01 15:56:46 -0500

    .
    .

--- public/admin/index.php
+++ public/admin/index.php
@@ -12,6 +12,8 @@ $query = "SELECT usuario.nombre,usuario.apellido,usuario.documento,usuario.corre
 
 $result = $conn->query($query);
 
+
+
 // Definir el número de resultados por página y la página actual
 $porPagina = 20; // Puedes ajustar esto según tus necesidades
 $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
@@ -19,11 +21,96 @@ $empieza = ($pagina - 1) * $porPagina;
 
 // Inicializa la variable $resultado_pagina
 $resultado_pagina = $result->fetchAll(PDO::FETCH_ASSOC);
+// Muestra el número de resultados obtenidos
+
+
+// Convierte los resultados a JSON para el console.log
+$userdata = json_encode($resultado_pagina);
+
+
+
 
 ?>
+<script>
+ 
+let dataTable;
+let dataTableIsInitialized = false;
+var json_data = <?php echo $userdata; ?>;
+
+const dataTableOptions = {
+    lengthMenu: [5, 10, 15, 20, 100, 200, 500],
+    columnDefs: [
+        { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
+        { orderable: false, targets: [5, 6] }, // Deshabilita la ordenación en las columnas 5 y 6
+        { searchable: true, targets: [0, 1] }  // Habilita explícitamente la búsqueda en las columnas 0 y 1
+    ],
+    pageLength: 3,
+    destroy: true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+};
 
 
+const initDataTable = async () => {
+    if (dataTableIsInitialized) {
+        dataTable.destroy();
+    }
 
+    await listUsers();
+
+    dataTable = $("#datatable_users").DataTable(dataTableOptions);
+
+    dataTableIsInitialized = true;
+};
+
+const listUsers = async () => {
+    try {
+        // Cambia la URL al archivo PHP que devuelve los datos de usuario
+       
+
+        let content = '';
+        json_data.forEach((json_data) => {
+            content += `
+                <tr>
+                    <td>${json_data.nombre}</td>
+                    <td>${json_data.apellido}</td>
+                    <td>${json_data.documento}</td>
+                    <td>${json_data.codigo_barras}</td>
+                    <td>${json_data.fecha_registro}</td>
+                    <td>${json_data.nom_forma}</td>
+                    <td>${json_data.ficha}</td>
+                    <td>${json_data.tp_jornada}</td>
+                    <td>
+                        <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+        });
+        document.getElementById('tableBody_users').innerHTML = content;
+    } catch (ex) {
+        alert(ex);
+    }
+};
+
+window.addEventListener("load", async () => {
+    await initDataTable();
+});
+
+console.log('Datos obtenidos:', json_data);
+</script>
 <div class="content-wrapper">
     <!-- Container-fluid starts -->
     <div class="container-fluid">
@@ -33,7 +120,6 @@ $resultado_pagina = $result->fetchAll(PDO::FETCH_ASSOC);
             <div class="row">
                 <div class="col-lg-12 p-0">
                     <div class="card-header">
-
                         <div class="content-body container-table">
                             <div class="container-fluid">
                                 <div class="row">
@@ -42,55 +128,47 @@ $resultado_pagina = $result->fetchAll(PDO::FETCH_ASSOC);
                                             <div class="card-header">
                                                 <h4 class="card-title">Actividad | Instructores</h4>
                                             </div>
-                                            <div class="card-body">
-                                                <div class="table-responsive">
-
-                                                    <table id="example" class="display" style="width:100%">
-                                                        <thead>
-                                                            <tr>
-                                                                <th>Nombre</th>
-                                                                <th>Apellido</th>
-                                                                <th>Documento</th>
-                                                                <th>Correo</th>
-                                                                <th>Código de Barras</th>
-                                                                <th>Fecha de Registro</th>
-                                                                <th>Formación</th>
-                                                                <th>Ficha</th>
-                                                                <th>Jornada</th>
-                                                                <th>Acciones</th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tfoot>
-                                                            <tr>
-                                                                <th>Nombre</th>
-                                                                <th>Apellido</th>
-                                                                <th>Documento</th>
-                                                                <th>Correo</th>
-                                                                <th>Código de Barras</th>
-                                                                <th>Fecha de Registro</th>
-                                                                <th>Formación</th>
-                                                                <th>Ficha</th>
-                                                                <th>Jornada</th>
-                                                                <th>Acciones</th>
-                                                            </tr>
-                                                        </tfoot>
-                                                    </table>
-
-
-
-
-
+                                            <div class="container my-4">
+                                                <div class="row">
+                                                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        <table id="datatable_users" class="table table-striped">
+                                                            <caption>
+                                                                DataTable.js Demo
+                                                            </caption>
+                                                            <thead>
+                                                                <tr>
+                                                                    <th class="centered">Nombre</th>
+                                                                    <th class="centered">Apellido</th>
+                                                                    <th class="centered">Documento</th>
+                                                                    <th class="centered">Codigo de Barras</th>
+                                                                    <th class="centered">Fecha de Registro</th>
+                                                                    <th class="centered">Formación</th>
+                                                                    <th class="centered">Ficha</th>
+                                                                    <th class="centered">Jornada</th>
+                                                                    <th class="centered">Acciones</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody id="tableBody_users"></tbody>
+                                                        </table>
+                                                    </div>
                                                 </div>
-                                                <!-- <div class="text-center" role="toolbar" aria-label="Toolbar with button groups">
-                                                        <div class="btn-group me-2" role="group" aria-label="First group" aling>
-                                                            <?php
-                                                            for ($i = 1; $i <= $total_paginas; $i++) {
-                                                                echo "<a class='btn btn-primary' href='index.php?pagina=" . $i . "'> " . $i . " </a>";
-                                                            }
-                                                            ?>
-                                                        </div>
-                                                    </div> -->
                                             </div>
+                                            <section class="d-flex align-items-center">
+                                                <div class="ml-3">
+                                                    <a href="excel.php" class="mr-2">
+                                                        <img src="../../images/excel1.png" alt="Imagen 2" class="img-fluid" style="max-height: 30px;">
+                                                    </a>
+                                                    <a href="excel.php" class="letra" style="font-size: 14px;">EXCEL</a>
+                                                </div>
+                                                <!-- Agregamos un espacio horizontal -->
+                                                <div style="width: 10px;"></div>
+                                                <div class="ml-3">
+                                                    <a href="pdf.php" class="mr-2">
+                                                        <img src="../../images/pdf1.png" alt="Imagen 1" class="img-fluid" style="max-height: 30px;">
+                                                    </a>
+                                                    <a href="pdf.php" class="letra" style="font-size: 14px;">PDF</a>
+                                                </div>
+                                            </section>
                                         </div>
                                     </div>
                                 </div>
@@ -101,69 +179,4 @@ $resultado_pagina = $result->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     </div>
-    <section class="d-flex align-items-center">
-        <div class="ml-3">
-            <a href="excel.php" class="mr-2">
-                <img src="../../images/excel1.png" alt="Imagen 2" class="img-fluid" style="max-height: 30px;">
-            </a>
-            <a href="excel.php" class="letra" style="font-size: 14px;">EXCEL</a>
-        </div>
-        <!-- Agregamos un espacio horizontal -->
-        <div style="width: 10px;"></div>
-        <div class="ml-3">
-            <a href="pdf.php" class="mr-2">
-                <img src="../../images/pdf1.png" alt="Imagen 1" class="img-fluid" style="max-height: 30px;">
-            </a>
-            <a href="pdf.php" class="letra" style="font-size: 14px;">PDF</a>
-        </div>
-    </section>
 </div>
-
-
-<script>
-    new DataTable('#example', {
-        ajax: './ajaxtable.php',
-        processing: true,
-        serverSide: true
-    });
-
-    $(document).ready(function() {
-        $('#example').DataTable({
-            "processing": true,
-            "serverSide": true,
-            "ajax": "ajaxtable.php",
-            "columns": [{
-                    "data": "nombre"
-                },
-                {
-                    "data": "apellido"
-                },
-                {
-                    "data": "documento"
-                },
-                {
-                    "data": "correo"
-                },
-                {
-                    "data": "codigo_barras"
-                },
-                {
-                    "data": "fecha_registro"
-                },
-                {
-                    "data": "nom_forma"
-                },
-                {
-                    "data": "ficha"
-                },
-                {
-                    "data": "tp_jornada"
-                },
-                {
-                    "data": null,
-                    "defaultContent": "<button>Acción</button>"
-                }
-            ]
-        });
-    });
-</script>
