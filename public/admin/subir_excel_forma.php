614a3583a8b41ac613caad597bc253c5fe1c4bf4
Your Name <jfcalderona16@gmail.com>
2024-05-12 00:01:59 -0500

    .

--- public/admin/subir_excel_forma.php
+++ public/admin/subir_excel_forma.php
@@ -1,60 +1,64 @@
 <?php
 require_once './../../database/conn.php';
-require_once './../../vendor/autoload.php'; // Incluye el archivo de autoload de PhpSpreadsheet
+require_once './../../vendor/autoload.php'; 
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
-
 
 $database = new Database();
 $conn = $database->conectar();
 session_start();
 date_default_timezone_set('America/Bogota');
 
-$nit= $_SESSION['nit_empre'];
+$nit = $_SESSION['nit_empre'];
 
-// Verifica si se ha enviado un archivo
 if (isset($_POST["submit"])) {
-    // Ruta donde se guardarán los archivos subidos
     $target_dir = "uploads/";
-    // Nombre del archivo subido
     $target_file = $target_dir . basename($_FILES["archivo_excel"]["name"]);
-    // Obtiene la extensión del archivo
     $file_extension = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // Verifica si el archivo es un archivo Excel válido
     if ($file_extension == "xls" || $file_extension == "xlsx") {
-        // Mueve el archivo desde la ubicación temporal al directorio de destino
         if (move_uploaded_file($_FILES["archivo_excel"]["tmp_name"], $target_file)) {
-            // Carga el archivo Excel
             $spreadsheet = IOFactory::load($target_file);
             $worksheet = $spreadsheet->getActiveSheet();
 
-            // Itera sobre las filas del archivo Excel
             foreach ($worksheet->getRowIterator() as $row) {
-                // Obtén los valores de cada celda en la fila
                 $cellIterator = $row->getCellIterator();
-                $cellIterator->setIterateOnlyExistingCells(false); // Permite iterar sobre todas las celdas, incluso si están vacías
+                $cellIterator->setIterateOnlyExistingCells(false); 
                 $data = [];
                 foreach ($cellIterator as $cell) {
-                    $data[] = $cell->getValue(); // Obtiene el valor de la celda
+                    $data[] = $cell->getValue(); 
                 }
 
-                // Inserta los datos en la base de datos
-                $sql = "INSERT INTO formacion (id_forma, nom_forma, nit_empre) VALUES (?, ?, '$nit')";
-                $stmt = $conn->prepare($sql);
-                // Suponiendo que los datos se encuentran en las columnas A y B, reemplaza los índices de $data con los índices de tus columnas
-                if ($stmt->execute([$data[0], $data[1]])) {
-                } else {
-                    echo '<script>alert("Hubo un error al subir el archivo.");';
+                // Verificar si ya existe un registro con los mismos valores en id_forma o nom_forma
+                $sql_check = "SELECT COUNT(*) AS count FROM formacion WHERE id_forma = ? OR nom_forma = ?";
+                $stmt_check = $conn->prepare($sql_check);
+                $stmt_check->execute([$data[0], $data[1]]);
+                $result_check = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+                if ($result_check['count'] > 0) {
+                    // Si ya existe, muestra un mensaje de error y no inserta el registro
+                    echo '<script>alert("El registro ya existe en la base de datos. Se el nombre de la formacion o si ingreso numeros en la primera columna tal vez tambien sea eso.");';
                     echo 'window.location = "./formacion.php";</script>';
+                } else {
+                    // Si no existe, procede con la inserción
+                    $sql_insert = "INSERT INTO formacion (id_forma, nom_forma, nit_empre) VALUES (?, ?, '$nit')";
+                    $stmt_insert = $conn->prepare($sql_insert);
+                    
+                    if ($stmt_insert->execute([$data[0], $data[1]])) {
+                        // Inserción exitosa
+                    } else {
+                        // Error al insertar
+                        echo '<script>alert("Hubo un error al subir el archivo.");';
+                        echo 'window.location = "./formacion.php";</script>';
+                    }
                 }
             }
 
             // Cierra la conexión a la base de datos
             $conn = null;
 
-            // echo '<script>alert("El archivo se ha subido y procesado correctamente.");';
-            // echo 'window.location = "./formacion.php";</script>';
+            echo '<script>alert("El archivo se ha subido y procesado correctamente.");';
+            echo 'window.location = "./formacion.php";</script>';
         } else {
             echo '<script>alert("Hubo un error al subir el archivo.");';
             echo 'window.location = "./formacion.php";</script>';
@@ -64,3 +68,4 @@ if (isset($_POST["submit"])) {
         echo 'window.location = "./formacion.php";</script>';
     }
 }
+?>
